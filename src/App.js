27a91df271d283af70269164d3b36c2a60bce98d
yyellen <yyopenfolder@gmail.com>
2023-01-17 15:23:54 +0800

    feat: create basic structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Todolist from "./components/Todolist"
 
 function App() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
+    <div className="w-screen h-screen flex justify-center items-center bg-gray-200">
       <Todolist/>
     </div>
   );
--- src/components/Todolist/index.js
+++ src/components/Todolist/index.js
@@ -1,8 +1,15 @@
 const Todolist = () => {
-
   return (
-    <div>TODO LIST</div>
+    <div className="flex flex-col items-center">
+      <h1 className="text-5xl">TODO LIST</h1>
+      <div className="mt-4 flex items-center">
+        <input className="p-2 h-8 border-[1px] rounded" type="text" />
+        <button className="w-8 h-8 bg-white border-[1px] rounded" type="button">+</button>
+      </div>
+      <div className="mt-4 border-[1px] w-64 h-72 bg-white">
+      </div>
+    </div>
   )
-}
+};
 
 export default Todolist;
