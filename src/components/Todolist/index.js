58009406f28326d5e3b7cc68a9a08c5de6e863aa
yyellen <yyopenfolder@gmail.com>
2023-01-18 15:48:32 +0800

    feat: add items.map to render todo item

--- src/components/Todolist/index.js
+++ src/components/Todolist/index.js
@@ -23,6 +23,15 @@ const Todolist = () => {
         <button className="w-8 h-8 bg-white border-[1px] rounded" type="button" onClick={addToList}>+</button>
       </div>
       <div className="mt-4 border-[1px] w-64 h-72 bg-white">
+        {
+          items.map((item, index) => {
+            return (
+              <div className="w-full h-8 flex items-center rounded bg-[#f0f0f0] mt-2 first:mt-0 p-2" key={index}>
+                {item}
+              </div>
+            )
+          })
+        }
       </div>
     </div>
   )
