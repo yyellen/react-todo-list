1f4bc8185dfef6c6fe1005c5e5dffc08dcccb28b
yyellen <yyopenfolder@gmail.com>
2023-01-18 15:17:58 +0800

    feat: add items array using useState, useEffect

--- src/components/Todolist/index.js
+++ src/components/Todolist/index.js
@@ -1,18 +1,25 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Todolist = () => {
   const inputRef = useRef(null);
+  const [items, setItems] = useState([]);
 
   const addToList = () => {
     const todoItem = inputRef.current.value;
     console.log(todoItem);
+    setItems([...items, todoItem]); // 非同步
+    inputRef.current.value = "";
   };
+
+  useEffect(() => {
+    console.log(items);
+  }, [items]);
 
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-5xl">TODO LIST</h1>
       <div className="mt-4 flex items-center">
-        <input ref={inputRef} className="p-2 h-8 border-[1px] rounded" type="text" />
+        <input ref={inputRef} className="p-2 h-8 border-[1px] rounded" type="text" placeholder="type some todo item..."/>
         <button className="w-8 h-8 bg-white border-[1px] rounded" type="button" onClick={addToList}>+</button>
       </div>
       <div className="mt-4 border-[1px] w-64 h-72 bg-white">
