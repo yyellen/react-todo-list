21c65af6b87df6fc691b0449e76d1fc036e0e63c
yyellen <yyopenfolder@gmail.com>
2023-01-19 16:12:43 +0800

    feat: add clearAll function to clear all todo items

--- src/components/Todolist/index.js
+++ src/components/Todolist/index.js
@@ -6,10 +6,14 @@ const Todolist = () => {
 
   const addToList = () => {
     const todoItem = inputRef.current.value;
-    console.log(todoItem);
+    // console.log(todoItem);
     setItems([...items, todoItem]); // 非同步
     inputRef.current.value = "";
   };
+
+  const clearAll = () => {
+    setItems([]);
+  }
 
   useEffect(() => {
     console.log(items);
@@ -22,17 +26,18 @@ const Todolist = () => {
         <input ref={inputRef} className="p-2 h-8 border-[1px] rounded" type="text" placeholder="type some todo item..."/>
         <button className="w-8 h-8 bg-white border-[1px] rounded" type="button" onClick={addToList}>+</button>
       </div>
-      <div className="mt-4 border-[1px] w-64 h-72 bg-white">
+      <div className="mt-4 p-4 border-[1px] w-64 bg-white">
         {
           items.map((item, index) => {
             return (
-              <div className="w-full h-8 flex items-center rounded bg-[#f0f0f0] mt-2 first:mt-0 p-2" key={index}>
+              <div className="w-full h-8 flex items-center rounded bg-[#f0f0f0] mt-2 first:mt-0 p-2 justify-between" key={index}>
                 {item}
               </div>
             )
           })
         }
       </div>
+      <button className="w-full h-8 flex items-center justify-center rounded bg-white mt-2" type="button" onClick={clearAll}>CLEAR ALL TODO</button>
     </div>
   )
 };
