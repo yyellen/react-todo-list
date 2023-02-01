6efdc78ae7b909d19e2d0af73647b9f8cd04ed30
yyellen <yyopenfolder@gmail.com>
2023-02-01 11:37:37 +0800

    feat: update addTodo function to change todoItem into an object with completed and editing status

--- src/components/Todolist/index.js
+++ src/components/Todolist/index.js
@@ -4,22 +4,25 @@ const Todolist = () => {
   const inputRef = useRef();
   const [todos, setTodos] = useState([]);
 
-  const addTodo = () => {
-    const todoItem = inputRef.current.value;
-    // console.log(todoItem);
-    setTodos([...todos, todoItem]);
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([...todos, {
+      text: inputRef.current.value,
+      completed: false,
+      editing: false
+    }]);
     inputRef.current.value = "";
   };
 
   const removeTodo = (indexToRemove) => {
-    // console.log(indexToRemove);
     setTodos(prevTodos => {
       return prevTodos.filter((_, index) => index !== indexToRemove);
       // 因為這個函數主要是檢查數組項目的索引，並不需要使用項目的實際值，因此使用空白識別符 _ 可以清晰地表明代碼的意圖
+      // prevTodos 代表在當前狀態下(函數執行之前)的待辦事項列表
     })
   }
 
-  const clearAll = () => {
+  const clearAllTodo = () => {
     setTodos([]);
   }
 
@@ -30,24 +33,24 @@ const Todolist = () => {
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-5xl">TODO LIST</h1>
-      <div className="mt-4 flex items-center">
+      <form className="mt-4 flex items-center" onSubmit={addTodo}>
         <input type="text" ref={inputRef} className="p-2 h-8 border-[1px] rounded" placeholder="type some todo item..."/>
-        <button type="button" className="w-8 h-8 bg-white border-[1px] rounded" onClick={addTodo}>+</button>
-      </div>
+        <button type="submit" className="w-8 h-8 bg-white border-[1px] rounded">+</button>
+      </form>
       <div className="mt-4 p-4 border-[1px] w-64 bg-white">
         {
           todos.map((todo, index) => {
             return (
               <div className="w-full h-8 flex items-center rounded bg-[#f0f0f0] mt-2 first:mt-0 p-2 justify-between" key={index}>
                 <input type="checkbox" className="checkBox" />
-                {todo}
+                {todo.text}
                 <button type="button" className="w-6 h-6 p-0 flex justify-center items-center bg-white border-[1px] rounded-full" onClick={() => removeTodo(index)}>x</button>
               </div>
             )
           })
         }
       </div>
-      <button type="button" className="w-full h-8 flex items-center justify-center rounded bg-white mt-2" onClick={clearAll}>CLEAR ALL TODO</button>
+      <button type="button" className="w-full h-8 flex items-center justify-center rounded bg-white mt-2" onClick={clearAllTodo}>CLEAR ALL TODO</button>
     </div>
   )
 };
